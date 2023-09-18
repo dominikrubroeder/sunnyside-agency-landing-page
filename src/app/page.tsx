4cc9b82aa42bf6ab99dec9e951f0645619ab758d
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-09-18 16:51:26 +0200

    Build intro UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import ArrowDown from "@/components/ArrowDown";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-fraunces">We are creatives</h1>
+    <main>
+      <section className="relative min-h-screen w-full pt-40 text-center">
+        <div className="relative z-10 flex flex-col items-center gap-24 text-[3.5rem] text-white">
+          <h1 className="font-fraunces uppercase leading-normal tracking-widest">
+            We are creatives
+          </h1>
+          <ArrowDown />
+        </div>
+        <Image
+          src="/header.jpg"
+          alt="Orange on blue background"
+          layout="fill"
+          objectFit="cover"
+        />
+      </section>
     </main>
-  )
+  );
 }
---
+++ src/components/ArrowDown.tsx
@@ -0,0 +1,26 @@
+export default function ArrowDown() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="36"
+      height="115"
+      viewBox="0 0 36 115"
+      fill="none"
+    >
+      <path
+        d="M18 3.54028V103.54"
+        stroke="white"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 96.0245L18 111.025L33 96.0245"
+        stroke="white"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
