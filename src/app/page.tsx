a2839aae75d5617a108834c1b1220a44347adaad
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-09-23 19:06:29 +0200

    Build yellow section UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,35 @@ export default function Home() {
           objectFit="cover"
         />
       </section>
+
+      <section className="grid min-h-[37.5rem] grid-cols-2">
+        <div className="mx-auto flex max-w-[27.8175rem] flex-col justify-center gap-8 text-left">
+          <h2 className="font-fraunces text-[2.5rem] leading-[1.2]">
+            Transform your brand
+          </h2>
+          <p>
+            We are a full-service creative agency specializing in helping brands
+            grow fast. Engage your clients through compelling visuals that do
+            most of the marketing for you.
+          </p>
+          <a
+            href=""
+            title="Learn more"
+            className="relative self-baseline pl-2 pr-2 font-fraunces uppercase transition-all duration-300 after:absolute after:bottom-0 after:left-0 after:-z-10 after:h-4 after:w-full after:rounded-full after:bg-[#FAD400] hover:pl-4 hover:pr-4"
+          >
+            Learn more
+          </a>
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/transform.jpg"
+            alt="Transform"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </section>
     </main>
   );
 }
