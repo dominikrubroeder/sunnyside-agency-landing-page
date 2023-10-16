ff07c47c48be5c5a1d4dbe723fe6cc584758bedb
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-10-16 11:58:12 +0200

    Build mobile Navigation UI and logic, Refactor for code splitting

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+const data = [
+  {
+    title: "About",
+    href: "about",
+  },
+  {
+    title: "Services",
+    href: "services",
+  },
+  {
+    title: "Projects",
+    href: "projects",
+  },
+];
+
+export default function Navigation() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const navigation = data.map(({ title, href }, index) => (
+    <li key={index} className="list-none text-center md:text-left">
+      <Link href={`#${href}`}>{title}</Link>
+    </li>
+  ));
+
+  return (
+    <>
+      <div className="hidden items-center gap-2 md:flex">
+        <nav>
+          <ul className="flex items-center gap-4">{navigation}</ul>
+        </nav>
+
+        <button className="rounded-full bg-white px-8 py-4 uppercase text-black transition hover:bg-white/25">
+          Contact
+        </button>
+      </div>
+
+      <div className="relative md:hidden">
+        <button
+          className="group grid gap-1.5"
+          onClick={() => setShowMobileMenu((previousState) => !previousState)}
+        >
+          <span className="h-1 w-6 bg-[#979797] group-hover:bg-white"></span>
+          <span className="h-1 w-6 bg-[#979797] group-hover:bg-white"></span>
+          <span className="h-1 w-6 bg-[#979797] group-hover:bg-white"></span>
+        </button>
+
+        {showMobileMenu && (
+          <div className="absolute right-0 top-12 grid w-80 justify-center gap-8 bg-white p-8 text-gray-400">
+            {navigation}
+
+            <button className="w-min rounded-full bg-yellow-400 px-8 py-4 uppercase text-black transition">
+              Contact
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,13 @@
 import Logo from "@/components/svg/Logo";
-import Link from "next/link";
+import Navigation from "@/components/Navigation";
 
 export default function Header() {
   return (
     <header className="absolute left-0 right-0 top-0 z-50 flex items-center justify-between gap-4 px-12 py-8 text-white">
-      <div>
-        <Logo />
-      </div>
+      <Logo />
 
       <div className="flex items-center gap-4">
-        <nav>
-          <ul className="flex items-center gap-4">
-            <li>
-              <Link href="#about">About</Link>
-            </li>
-            <li>
-              <Link href="#services">Services</Link>
-            </li>
-            <li>
-              <Link href="#projects">Projects</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <button className="rounded-full bg-white px-8 py-4 uppercase text-black transition hover:bg-white/25">
-          Contact
-        </button>
+        <Navigation />
       </div>
     </header>
   );
