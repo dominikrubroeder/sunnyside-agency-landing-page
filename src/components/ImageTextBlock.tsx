3721e412f02682fd95e2755b16690b1d0a8fc400
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-10-16 15:51:35 +0200

    Optimize responsive usage for ImageTextBlock component

--- src/components/ImageTextBlock.tsx
+++ src/components/ImageTextBlock.tsx
@@ -13,10 +13,24 @@ export default function ImageTextBlock({
   text,
 }: ImageTextSectionProps) {
   return (
-    <section className="grid min-h-[37.5rem] grid-cols-2">
+    <section className="flex flex-col md:grid md:min-h-[37.5rem] md:grid-cols-2">
       <div
-        className={`mx-auto flex max-w-[27.8175rem] flex-col justify-center gap-8 text-left ${
-          imagePosition === "Left" ? "order-2" : "order-1"
+        className={`relative h-80 shrink-0 md:h-auto ${
+          imagePosition === "Left" ? "md:order-1" : "md:order-2"
+        }`}
+      >
+        <Image
+          src={imageSource}
+          alt="Transform"
+          layout="fill"
+          className="shrink-0"
+          objectFit="cover"
+        />
+      </div>
+
+      <div
+        className={`mx-auto flex max-w-[27.8175rem] flex-col justify-center gap-8 px-6 py-16 text-left ${
+          imagePosition === "Left" ? "md:order-2" : "md:order-1"
         }`}
       >
         <h2 className="font-fraunces text-[2.5rem] leading-[1.2]">{title}</h2>
@@ -28,19 +42,6 @@ export default function ImageTextBlock({
         >
           Learn more
         </a>
-      </div>
-
-      <div
-        className={`relative ${
-          imagePosition === "Left" ? "order-1" : "order-2"
-        }`}
-      >
-        <Image
-          src={imageSource}
-          alt="Transform"
-          layout="fill"
-          objectFit="cover"
-        />
       </div>
     </section>
   );
